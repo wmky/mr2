fa48c5fb43d3a980eddcdf409904e42a503805a9
youck <youck@jiguang.cn>
2018-08-24 17:00:28 +0800

    [add]字符串切割数组后存放map中

--- src/main/java/cn/jpush/util/ArrUtil.java
+++ src/main/java/cn/jpush/util/ArrUtil.java
@@ -8,10 +8,11 @@ public class ArrUtil {
      * 将传入的数组转化为key-value的xing
      * @param
      */
-    public static void ArrToMap(String complexStr,Map<String,String> map) {
+    public static Map<String,String> ArrToMap(String complexStr) {
         //去掉换行符
         String key = "";
         String value = "";
+        Map<String,String> map = new HashMap<String,String>();
         String[] arr = complexStr.split("\\&");
         for (int i=0;i<arr.length;i++) {
             //判断字符串中有没有点等于号
@@ -24,11 +25,16 @@ public class ArrUtil {
                 map.put(key,value);
             }
         }
+        return map;
     }
     public static void main(String[] args){
-        HashMap<String,String> map = new HashMap<String,String>();
+//        HashMap<String,String> map = new HashMap<String,String>();
         String line = "eId=M70211584&obj={\"cityCode\":\"000002\",\"priceTags\":\"3000-6000\",\"rooms\":\"2\",\"includeFrontImage\":1,\"pageNo\":1,\"pageSize\":20}&bd=iPhone&osv=10.3.1&uid=";
-        ArrToMap(line,map);
-        System.out.println(map.get("eId"));
+        Map<String,String> map = ArrToMap(line);
+        System.out.println("eId = " + map.get("eId"));
+        System.out.println("obj = " + map.get("obj"));
+        System.out.println("bd = " + map.get("bd"));
+        System.out.println("osv = " + map.get("osv"));
+        System.out.println("uid = " + map.get("uid"));
     }
 }
